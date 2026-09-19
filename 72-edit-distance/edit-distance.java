acc4524abe3bd3f203beb5836f56aa17db010525
Aamin Khan <aaminkhan2804@gmail.com>
2026-09-19 23:53:14 +0530

    Time: 5 ms (65.98%) | Memory: 47 MB (70.49%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,27 @@
+class Solution {
+     //Mraaminkhan
+    public int minDistance(String word1, String word2) {
+        int row = word1.length();
+        int col = word2.length();
+        int [][] dp = new int[row+1][col+1];
+        int i,j;
+    for(i=0;i<=col;i++){
+        dp[0][i]=i;
+    }
+    for(i=0;i<=row;i++){
+        dp[i][0]=i;
+    }
+    for(i=1;i<=row;i++){
+        for(j=1;j<=col;j++){
+            if(word1.charAt(i-1) == word2.charAt(j-1)){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+    }
+    return dp[row][col];
+        
+    }
+}
